ebbe90a4c49776b925adb428741cba915d0a68c7
Juan Espinoza <juan.espinoza@fluxit.com.ar>
2020-06-28 18:49:50 -0300

    Fix code

--- server/server.js
+++ server/server.js
@@ -13,39 +13,8 @@ let server = http.createServer(app);
 app.use(express.static(publicPath));
 
 //Inicialitized backend communication with Socket.Io
-let io = socketIo(server);
-//Inicialitized communication with frontend
-//The client parameter contains information about the connection established
-io.on('connection', (client) => {
-    console.log('User connected');
-
-    client.on('disconnect', () => {
-        console.log('Disconnected user');
-    });
-
-    //Listenning the client
-    //The callback is received from Client
-    client.on('sendMessage', (message, callback) => {
-        console.log(message);
-        if (message.user) {
-            callback({
-                res: 'All went well'
-            });
-        } else {
-            callback({
-                res: 'Everything went wrong'
-            });
-        }
-    });
-
-    //Function "emit" is for send information only to Client
-    client.emit('sendMessage', {
-        user: 'Admin',
-        message: 'Hello to this app'
-    });
-
-});
-
+module.exports.io = socketIo(server);
+require('./sockets/socket');
 
 server.listen(port, (err) => {
 
---
+++ server/sockets/socket.js
@@ -0,0 +1,33 @@
+const io = require('../server');
+
+//Inicialitized communication with frontend
+//The client parameter contains information about the connection established
+io.on('connection', (client) => {
+    console.log('User connected');
+
+    client.on('disconnect', () => {
+        console.log('Disconnected user');
+    });
+
+    //Listenning the client
+    //The callback is received from Client
+    client.on('sendMessage', (message, callback) => {
+        console.log(message);
+        if (message.user) {
+            callback({
+                res: 'All went well'
+            });
+        } else {
+            callback({
+                res: 'Everything went wrong'
+            });
+        }
+    });
+
+    //Function "emit" is for send information only to Client
+    client.emit('sendMessage', {
+        user: 'Admin',
+        message: 'Hello to this app'
+    });
+
+});
